8f4ecaf39d314962f90699e1e9796e37f3c6b6da
Jinxlpa <jinx_lpa@163.com>
2016-12-07 13:51:38 +0800

    Location

---
+++ app/src/main/java/com/xyproject/xyproject/utils/location/MyOverlay.java
@@ -0,0 +1,15 @@
+package com.xyproject.xyproject.utils.location;
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.model.LatLng;
+
+/**
+ * Created by hasee on 2016/11/26.
+ */
+public class MyOverlay {
+
+    public static void setOverlay(BaiduMap map, LatLng latLng){
+        
+    }
+
+}
